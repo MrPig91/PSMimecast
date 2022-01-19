b67a23cf8e6445a5ddbb09b7135cf501cf54c212
MrPig91 <sirius.cleveland@gmail.com>
2022-01-18 21:16:42 -0600

    added help to Lock-MimecastAccount

--- PSMimecast/Public/Lock-MimecastAccount.ps1
+++ PSMimecast/Public/Lock-MimecastAccount.ps1
@@ -1,4 +1,33 @@
+<#
+.SYNOPSIS
+    This function will lock a Mimecast user account.
+.DESCRIPTION
+    This function will lock a Mimecast user account.
+.PARAMETER EmailAddress
+    This parameter specifies the emaill address of the account you want to lock.
+.EXAMPLE
+    PS C:\> Lock-MimecastAccount -EmailAddress syrius.cleveland@example.com
+
+    emailAddress         : syrius.cleveland@example.com
+    name                 : Syrius Cleveland
+    forcePasswordChange  : False
+    passwordNeverExpires : False
+    accountLocked        : True
+    accountDisabled      : False
+    allowSmtp            : False
+    allowPop             : False
+
+    This example locks the account with the email address syrius.cleveland@example.com.
+.INPUTS
+    None
+.OUTPUTS
+    PSCustomObject
+.NOTES
+    This function uses the update-user API call to lock the user account.
+#>
 function Lock-MimecastAccount{
+    [cmdletbinding()]
+    [Alias("Lock-mcAccount")]
     param(
         [Parameter(Mandatory)]
         $EmailAddress

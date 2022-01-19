4d24f96cf86e23a0c50427f8563249a2329e307a
MrPig91 <sirius.cleveland@gmail.com>
2022-01-18 21:09:46 -0600

    Added help to Get-MimecasetAppInfo

--- PSMimecast/Public/Get-MimecastAppInfo.ps1
+++ PSMimecast/Public/Get-MimecastAppInfo.ps1
@@ -1,3 +1,24 @@
+<#
+.SYNOPSIS
+    This function retrieves the app info used to communicate with Mimecast's API that was previously stored using Set-MimecastAppInfo.
+.DESCRIPTION
+    This function retrieves the app info used to communicate with Mimecast's API that was previously stored using Set-MimecastAppInfo.
+    This information is stored in the following path, "$ENV:APPDATA\PSMimecast\AppInfo.xml".
+.EXAMPLE
+    PS C:\> Get-MimecastAppInfo
+
+    AppId                                AppKey
+    -----                                ------
+    XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+
+    This example show the only way to use this function. The app info was successfully retrieved. Actual values have been redacted for this example.
+.INPUTS
+    None
+.OUTPUTS
+    PSCustomObject
+.NOTES
+    App info must have been previously set using Set-MimecastAppInfo.
+#>
 function Get-MimecastAppInfo {
     $Path = "$ENV:APPDATA\PSMimecast\AppInfo.xml"
     if (Test-Path -Path $Path){
